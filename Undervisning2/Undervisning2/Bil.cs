bcf0cb23bcb07903387c19d8de5c021d4c6f8c70
marie <marie@getacademy.no>
2023-05-04 13:49:40 +0200

    økt 2

---
+++ Undervisning2/Undervisning2/Bil.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Undervisning2
+{
+    /*
+             let bil = {
+                motor: 'V8',
+                registreringsnr: 'BJ69693',
+                farge: 'blå',
+                wunderbaum: true
+            }
+                          
+             */
+    internal class Bil
+    {
+        
+        //string _registreringsnr = "BJ69693";
+        //string _farge = "blå";
+        //bool _harWunderbaum = true;
+
+        public string Motor { get; private set; }
+
+        public void PutInEngine(string engineType)
+        {
+            Motor = engineType;
+        }
+
+
+    }
+}
---
+++ Undervisning2/Undervisning2/Program.cs
@@ -0,0 +1,102 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics.Metrics;
+
+namespace Undervisning2
+{
+        class Program
+        {
+            static Random Random = new Random();
+            
+            static void Main(string[] args)
+            {              
+                Bil bil = new Bil();
+                bil.PutInEngine("V8");
+                Console.WriteLine(bil.Motor);
+                //if(args.Length != 2)
+                //{
+                //    ShowMenu();
+                //}
+                //else 
+                //{
+                //    if (IsArgumentsValid(args))
+                //    {
+                //        string pattern = args[1];
+
+            //        int totalCharacters = int.Parse(args[0]);
+            //       if(totalCharacters < 14)
+            //        {
+            //            pattern = pattern.PadRight(totalCharacters, 'l');
+            //        }
+
+            //    }
+            //    else
+            //    {
+            //        ShowMenu();
+            //    }
+            //}
+            //var randomLetters = new char[10];
+            //var randomNumbers = new int[10];
+
+            //for (var i = 0; i < 10; i++)
+            //{
+            //    var randomLetter = GetRandomUpperCaseLetter();
+            //    var randomNumber = Random.Next(0, 10);
+
+            //    randomLetters[i] = randomLetter;
+            //    randomNumbers[i] = randomNumber;
+            //}
+        }
+
+        static bool IsArgumentsValid(string[] args)
+        {
+            var passwordLength = args[0];
+            foreach (var character in passwordLength)
+            {
+                var isdigit = char.IsDigit(character);
+                if(isdigit == false)
+                {
+                    return false;
+                }
+            }
+
+            var options = args[1];
+            foreach (var character in options)
+            {
+                if(!(character == 'l' || character == 'L' || character == 'd' || character == 's'))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static void ShowMenu()
+        {
+            var menustring = @"PasswordGenerator
+                  Options:
+                  -l = lower case letter
+                  - L = upper case letter
+                  - d = digit
+                  - s = special character(!""#¤%&/(){}[]
+                Example: PasswordGenerator 14 lLssdd
+                         Min. 1 lower case
+                         Min. 1 upper case
+                         Min. 2 special characters
+                         Min. 2 digits";
+            Console.WriteLine(menustring);
+        }
+                    
+
+            private static char GetRandomUpperCaseLetter()
+            {
+                return GetRandomLetter('A', 'Z');
+            }
+
+            private static char GetRandomLetter(char min, char max)
+            {
+                return (char)Random.Next(min, max);
+            }
+        }
+
+}
