df4abeab5f496fbfa86bbf0ea73d8cd25244df33
Marie Askjem <Marie@GetAcademy.no>
2023-05-11 14:04:05 +0200

    socialmediafinishedwithbeep

--- Undervisning3/Friendface/Profile.cs
+++ Undervisning3/Friendface/Profile.cs
@@ -14,19 +14,40 @@ namespace annetNamespace
         public string Address { get; set; }
         public List<Profile> Friends { get; set; }
      
-        public Profile(string name)
+        public Profile(string name, int age, string relationshipstatus)
         {
             Name = name;
             Friends = new List<Profile>();
+            Age = age;
+            RelationshipStatus = relationshipstatus;
+        }
+
+        public void PrintProfileInfo()
+        {
+            Console.WriteLine($"Name: {Name} Age: {Age} Relationshipstatus {RelationshipStatus}");
+        }
+
+        public void PrintFriends()
+        {
+            for (var i = 0; i < Friends.Count; i++)
+            {
+                Console.WriteLine($"{i} {Friends[i].Name}");
+            }
         }
         public void AddFriend(Profile friendToAdd)
         {
             Friends.Add(friendToAdd);
         }
 
-        public void RemoveFriend()
+        public void RemoveFriend(int index)
         {
+            Friends.RemoveAt(index);
+        }
 
+        public Profile GetSelectedFriend(int index)
+        {
+            var friend = Friends[index];
+            return friend;
         }
 
     }
--- Undervisning3/Friendface/Program.cs
+++ Undervisning3/Friendface/Program.cs
@@ -1,13 +1,87 @@
 ﻿
 using annetNamespace;
 
-Console.WriteLine("Hello, World!");
 var socialMediaAccounts = new List<Profile>();
-socialMediaAccounts.Add(new Profile("Bjarne"));
-socialMediaAccounts.Add(new Profile("Kurt"));
-socialMediaAccounts.Add(new Profile("Stig"));
-socialMediaAccounts.Add(new Profile("Nils"));
+InitializeAccounts();
 
 //Innlogget bruker
-var mainProfile = new Profile("Kåre");
-mainProfile.AddFriend(socialMediaAccounts[0]);
+var mainProfile = new Profile("Kåre", 25, "single");
+RunShit();
+
+void RunShit()
+{
+    bool isRunning = true;
+    while (isRunning)
+    {
+        PrintMenu();
+        Console.Beep();
+        Console.WriteLine();
+        var input = Console.ReadLine();
+
+        if (input == "1")
+        {
+            AddFriendMenu();
+        }
+        else if (input == "2")
+        {
+            RemoveUserMenu();
+        }
+        else if (input == "3")
+        {
+            mainProfile.PrintFriends();
+        }
+        else if (input == "4")
+        {
+            PrintFriendInfoMenu();
+        }
+        else
+        {
+            isRunning = false;
+        }
+    }
+}
+void PrintMenu()
+{
+    Console.WriteLine($"Welcome to Friendface {mainProfile.Name}");
+    Console.WriteLine("1. Add friend");
+    Console.WriteLine("2. Remove friend");
+    Console.WriteLine("3. Print friends");
+    Console.WriteLine("4. Show friend info");
+}
+void RemoveUserMenu()
+{
+    Console.WriteLine("Please select the user you want to remove");
+    mainProfile.PrintFriends();
+    var chosenIndex = int.Parse(Console.ReadLine());
+    mainProfile.RemoveFriend(chosenIndex);
+}
+
+void PrintUsers()
+{
+    for (var i = 0; i < socialMediaAccounts.Count; i++)
+    {
+        Console.WriteLine($"{i} {socialMediaAccounts[i].Name}");
+    }
+}
+void AddFriendMenu()
+{
+    Console.WriteLine("Please select the user you want to add");
+    PrintUsers();
+    var chosenIndex = int.Parse(Console.ReadLine());
+    var profileToAdd = socialMediaAccounts[chosenIndex];
+    mainProfile.AddFriend(profileToAdd);
+}
+void PrintFriendInfoMenu()
+{
+    mainProfile.PrintFriends();
+    var profileIndex = int.Parse(Console.ReadLine());
+    var selectedFriend = mainProfile.GetSelectedFriend(profileIndex);
+    selectedFriend.PrintProfileInfo();
+}
+void InitializeAccounts()
+{
+    socialMediaAccounts.Add(new Profile("Bjarne", 25, "single"));
+    socialMediaAccounts.Add(new Profile("Kurt", 25, "single"));
+    socialMediaAccounts.Add(new Profile("Stig", 25, "single"));
+    socialMediaAccounts.Add(new Profile("Nils", 25, "single"));
+}
